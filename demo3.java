64cdb09983cd3c3d7441407d53f9748aaad6ddb4
SuprajaNandyala <suprajan02@gmail.com>
2022-09-29 10:05:32 +0530

    master commit

--- demo3.java
+++ demo3.java
@@ -1,9 +1,10 @@
-class demo{
+
+class demo3{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int i;
         i=sc.nextInt();
         System.out.println(i);
-        System.out.println("demo");
+        System.out.println("demo3");
     }
 }
